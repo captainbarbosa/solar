bb07682540e50b584dda93090b2774da9a639e5a
Nadia Barbosa <captainbarbosa@users.noreply.github.com>
2017-07-16 20:12:34 -0400

    Initial Commit

---
+++ solar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  solar
+//
+//  Created by Nadia Barbosa on 7/16/17.
+//  Copyright © 2017 nb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
