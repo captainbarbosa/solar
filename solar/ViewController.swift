4773d5f438cdae3b4018bfbfd783246339b94e9e
Nadia Barbosa <captainbarbosa@users.noreply.github.com>
2017-08-20 11:56:09 -0400

    Center on each selected feature 🙌

--- solar/ViewController.swift
+++ solar/ViewController.swift
@@ -12,7 +12,7 @@ import Mapbox
 
 class ViewController: UIViewController, MGLMapViewDelegate {
     var timer: Timer?
-    var utcSecTime = 61979
+    var utcSecTime = 61920
     var vectorSource: MGLVectorSource!
     
     @IBOutlet weak var mapView: MGLMapView!
@@ -24,47 +24,51 @@ class ViewController: UIViewController, MGLMapViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        mapView.setCenter(CLLocationCoordinate2D(latitude: 39.23225, longitude: -97.91015), zoomLevel: 5, animated: false)
+        mapView.setCenter(CLLocationCoordinate2D(latitude: 39.23225, longitude: -97.91015), zoomLevel: 2, animated: false)
         mapView.delegate = self
+        
+        timeView.layer.cornerRadius = timeView.frame.size.height / 8
     }
     
     func mapView(_ mapView: MGLMapView, didFinishLoading style: MGLStyle) {
-        
-        vectorSource = MGLVectorSource(identifier: "eclipse-umbra", configurationURL: URL(string: "mapbox://nbb12805.a9ot0w9k")!)
+        vectorSource = MGLVectorSource(identifier: "eclipse-umbra", configurationURL: URL(string: "mapbox://nbb12805.7nlrnej6")!)
         style.addSource(vectorSource)
         
         let layer = MGLFillStyleLayer(identifier: "eclipse-umbra-style", source: vectorSource)
-        layer.sourceLayerIdentifier = "sixtysec"
+        layer.sourceLayerIdentifier = "60s_umbrageojson"
         
         style.addLayer(layer)
+    }
+    
+    func mapViewDidFinishLoadingMap(_ mapView: MGLMapView) {
         animateUmbraSelection()
     }
+    
+    func animateUmbraSelection() {
+        _ = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true, block: {_ in
+            self.utcSecTime += 60
             
-    @objc func tick() {
-        
-        utcSecTime += 60
-        print(utcSecTime)
-        
-        let categoricalStops = [
-            utcSecTime: MGLStyleValue<UIColor>(rawValue: #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1))
-        ]
-        
-        if let layer = mapView.style?.layer(withIdentifier: "eclipse-umbra-style") as? MGLFillStyleLayer {
-            layer.fillColor = MGLStyleValue(interpolationMode: .categorical, sourceStops: categoricalStops, attributeName: "UTCSec", options: [.defaultValue: MGLStyleValue<UIColor>(rawValue: #colorLiteral(red: 1, green: 0.3883662726, blue: 0.278445029, alpha: 0.8107074058))])
+            let categoricalStops = [
+                self.utcSecTime: MGLStyleValue<UIColor>(rawValue: #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1))
+            ]
             
-        }
+            if let layer = self.mapView.style?.layer(withIdentifier: "eclipse-umbra-style") as? MGLFillStyleLayer {
+                layer.fillColor = MGLStyleValue(interpolationMode: .categorical, sourceStops: categoricalStops, attributeName: "UTCSec", options: [.defaultValue: MGLStyleValue<UIColor>(rawValue: #colorLiteral(red: 1, green: 0.3883662726, blue: 0.278445029, alpha: 0.8107074058))])
+            }
+            
+            let predicate = NSPredicate(format: "UTCSec = %i", self.utcSecTime)
+            
+            let selectedFeature = self.vectorSource.features(sourceLayerIdentifiers: Set(["60s_umbrageojson"]), predicate: predicate)
+            
+            guard let latitude = selectedFeature.first?.attribute(forKey: "CenterLat") as? NSNumber else { return }
+            
+            guard let longitude = selectedFeature.first?.attribute(forKey: "CenterLon") as? NSNumber else { return }
+            
+            self.mapView.centerCoordinate = CLLocationCoordinate2D(latitude: latitude.doubleValue, longitude: longitude.doubleValue)
+            
+            self.timeLabel.text = "\(self.timeFormatted(utcSec: self.utcSecTime))"
+        })
         
-        // ----- not working
-        let num = 61979
-        let predicate = NSPredicate(format: "UTCSec == %i", num)
-        
-        let selectedFeature = vectorSource.features(sourceLayerIdentifiers: Set(["sixtysec"]), predicate: predicate)
-        // ------------------
-        
-        // TODO: center camera on selected feature
-        
-        
-        timeLabel.text = "\(timeFormatted(utcSec: utcSecTime))"
     }
     
     func timeFormatted(utcSec: Int) -> String {
@@ -87,10 +91,6 @@ class ViewController: UIViewController, MGLMapViewDelegate {
         let convertedDate = dateFormatter.string(from: dateTime as Date)
         
         return "\(convertedDate)"
-    }
-    
-    func animateUmbraSelection() {
-        timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(tick), userInfo: nil, repeats: true)
     }
 }
 
